d593b1fbd809e5adb229d298a86fb2b69b77983e
Sun-ny11 <spetrow79@gmail.com>
2024-05-27 13:12:36 +0300

    test PR

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -10,6 +10,8 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
+// test
+
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
   const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
