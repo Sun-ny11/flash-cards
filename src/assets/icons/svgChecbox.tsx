fc39bd75a6e77e40991d6b71a68451008750ac95
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-05-28 22:17:52 +0300

    add icon checkbox

---
+++ src/assets/icons/svgChecbox.tsx
@@ -0,0 +1,19 @@
+export const Check = () => {
+  return (
+    <svg
+      fill={'none'}
+      height={'12'}
+      viewBox={'0 0 16 12'}
+      width={'16'}
+      xmlns={'http://www.w3.org/2000/svg'}
+    >
+      <path
+        d={'M1.00002 5.5L4.33336 9.00001L14 1.33334'}
+        stroke={'#2D3748'}
+        strokeLinecap={'round'}
+        strokeLinejoin={'round'}
+        strokeWidth={'2'}
+      />
+    </svg>
+  )
+}
