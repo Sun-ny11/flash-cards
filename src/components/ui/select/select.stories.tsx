3e7ef92a9225e90bb0a1bce8093ac6ff1761b64d
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-05-29 10:24:20 +0300

    add select

---
+++ src/components/ui/select/select.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox } from './checkbox'
+
+const meta = {
+  argTypes: {
+
+  },
+  component: Checkbox,
+  tags: ['autodocs'],
+  title: 'Components/Checkbox',
+} satisfies Meta<typeof Checkbox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CheckBoxStory: Story = {
+  args: {
+    checked: true,
+    label: 'Hello',
+  },
+}
+export const CheckBoxStory1: Story = {
+  args: {
+    checked: false,
+
+    label: 'jjjj',
+  },
+}
+export const CheckBoxDisabled: Story = {
+  args: {
+    checked: false,
+    disabled: true,
+    label: 'HahAH',
+  },
+}
---
+++ src/components/ui/select/select.tsx
@@ -0,0 +1,38 @@
+import * as SelectRadix from '@radix-ui/react-select'
+
+type Props = {}
+
+export const Select = (props: Props) => {
+  return (
+    <SelectRadix.Root>
+      <SelectRadix.Trigger>
+        <SelectRadix.Value />
+        <SelectRadix.Icon />
+      </SelectRadix.Trigger>
+
+      <SelectRadix.Portal>
+        <SelectRadix.Content>
+          <SelectRadix.ScrollUpButton />
+          <SelectRadix.Viewport>
+            <SelectRadix.Item>
+              <SelectRadix.ItemText />
+              <SelectRadix.ItemIndicator />
+            </SelectRadix.Item>
+
+            <SelectRadix.Group>
+              <SelectRadix.Label />
+              <SelectRadix.Item>
+                <SelectRadix.ItemText />
+                <SelectRadix.ItemIndicator />
+              </SelectRadix.Item>
+            </SelectRadix.Group>
+
+            <SelectRadix.Separator />
+          </SelectRadix.Viewport>
+          <SelectRadix.ScrollDownButton />
+          <SelectRadix.Arrow />
+        </SelectRadix.Content>
+      </SelectRadix.Portal>
+    </SelectRadix.Root>
+  );
+};
