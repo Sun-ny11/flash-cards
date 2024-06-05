60db292b0a9e6596fab81f0b873022772260529d
Sun-ny11 <spetrow79@gmail.com>
2024-06-05 12:03:20 +0300

    component conrolledRadio is done

---
+++ src/components/ui/controlled/controlledRadioGroup/controlledRadioGroup.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useForm } from 'react-hook-form'
+
+import { ControlledRadioGroup } from '.'
+import { Button } from '../../button'
+
+const meta = {
+  component: ControlledRadioGroup,
+  tags: ['autodocs'],
+  title: 'Components/controlled/controlledRadioGroup',
+} satisfies Meta<typeof ControlledRadioGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Inputs: Story = {
+  args: {
+    items: [
+      { label: 'Did not know', value: '1' },
+      { label: 'Forgot', value: '2' },
+      { label: 'A lot of thought', value: '3' },
+      { label: 'Confused', value: '4' },
+      { label: 'Knew the answer', value: '5' },
+    ],
+    name: 'grade',
+  },
+
+  render: args => {
+    type Qwerty = { grade: string }
+
+    const { control, handleSubmit } = useForm<Qwerty>({
+      defaultValues: {
+        grade: '1',
+      },
+    })
+
+    const onSubmit = handleSubmit(data => {
+      console.log(data)
+    })
+
+    return (
+      <form onSubmit={onSubmit}>
+        <ControlledRadioGroup control={control} items={args.items} name={'grade'} />
+        <Button>Send</Button>
+      </form>
+    )
+  },
+}
---
+++ src/components/ui/controlled/controlledRadioGroup/controlledRadioGroup.tsx
@@ -0,0 +1,19 @@
+import { Control, FieldPath, FieldValues, useController } from 'react-hook-form'
+
+import { Options, RadioGroup } from '../../radioGroup'
+type Props<T extends FieldValues> = {
+  control?: Control<T>
+  items: Options[]
+  name: FieldPath<T>
+}
+export const ControlledRadioGroup = <T extends FieldValues>({ control, items, name }: Props<T>) => {
+  const {
+    field: { onChange, ...field },
+  } = useController({ control, name, rules: { required: true } })
+
+  return (
+    <>
+      <RadioGroup items={items} onValueChange={onChange} {...field} />
+    </>
+  )
+}
