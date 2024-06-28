d755c214aad50202f1d4f2b1c8219a6cb4e1dce3
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-06-28 16:11:46 +0400

    deckList/cards-#61 add search in DeckPage

--- src/features/tables/decks/ui/deckPage/deckPage.tsx
+++ src/features/tables/decks/ui/deckPage/deckPage.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { NavLink, useParams } from 'react-router-dom'
 
 import { ArrowBackOutline } from '@/assets/components'
 import { Button, Typography } from '@/components/ui'
+import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field'
 import { Pagination } from '@/components/ui/pagination'
 import { CardsList } from '@/features/tables/cards/ui/cardsList/cardsList'
 import { useGetCardsQuery, useGetDeckQuery } from '@/services/flashCardsApi'
@@ -12,12 +14,26 @@ export const DeckPage = () => {
   const { deckId } = useParams()
   const [currentPage, setCurrentPage] = useState(1)
   const [pageSize, setPageSize] = useState(10)
+  const [searchValue, setSearchValue] = useState<string>('')
   const { data: deckData, isLoading: deckDataIsLoading } = useGetDeckQuery(deckId || '')
   const { data: cardsData, isLoading: cardsDataIsLoading } = useGetCardsQuery({
     currentPage,
     id: deckId || '',
     itemsPerPage: pageSize,
   })
+
+  const { control } = useForm({
+    defaultValues: {
+      search: '',
+    },
+  })
+  let filteredCards = cardsData?.items
+
+  useEffect(() => {
+    filteredCards = cardsData?.items.filter(card => {
+      return card.question.toLowerCase().includes(searchValue.toLowerCase())
+    })
+  }, [searchValue])
 
   if (deckDataIsLoading || cardsDataIsLoading) {
     return <h2>Loading...</h2>
@@ -38,7 +54,20 @@ export const DeckPage = () => {
         <Button variant={'primary'}>Add new card</Button>
       </div>
       <img alt={'deck cover'} className={s.image} src={deckData?.cover} />
-      <CardsList isMy={false} items={cardsData?.items} />
+      <div className={s.search}>
+        <ControlledTextField
+          control={control}
+          label={'Search by question'}
+          name={'search'}
+          onValueChange={setSearchValue}
+          placeholder={'Search card...'}
+        />
+      </div>
+      {filteredCards ? (
+        <CardsList isMy={false} items={filteredCards} />
+      ) : (
+        <Typography>There is no cards in this deck!</Typography>
+      )}
       {cardsData && (
         <Pagination
           className={s.pagination}
